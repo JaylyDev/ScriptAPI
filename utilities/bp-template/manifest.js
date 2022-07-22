0bd38035d2f9b27f39bba6bd8a57eeb1ec9a1c93
Jayly <65847850+JaylyDev@users.noreply.github.com>
2022-07-22 12:25:13 +0100

    Update manifest.js

--- utilities/bp-template/manifest.js
+++ utilities/bp-template/manifest.js
@@ -25,27 +25,27 @@ const manifest = {
     {
       description: "mojang-gametest",
       uuid: "6f4b6893-1bb6-42fd-b458-7fa3d0c89616",
-      version: [0, 1, 0],
+      version: "1.0.0-beta",
     },
     {
       description: "mojang-minecraft",
       uuid: "b26a4d4c-afdf-4690-88f8-931846312678",
-      version: [0, 1, 0],
+      version: "1.0.0-beta",
     },
     {
       description: "mojang-minecraft-ui",
       uuid: "2bd50a27-ab5f-4f40-a596-3641627c635e",
-      version: [0, 1, 0],
+      version: "1.0.0-beta",
     },
     {
       description: "mojang-net",
       uuid: "777b1798-13a6-401c-9cba-0cf17e31a81b",
-      version: [0, 1, 0],
+      version: "1.0.0-beta",
     },
     {
       description: "mojang-minecraft-server-admin",
       uuid: "53d7f2bf-bf9c-49c4-ad1f-7c803d947920",
-      version: [0, 1, 0],
+      version: "1.0.0-beta",
     },
   ],
   modules: [
@@ -64,4 +64,4 @@ const manifest = {
   },
 };
 
-fs.writeFileSync(path.join(__dirname, "manifest.json"), JSON.stringify(manifest));
+fs.writeFileSync(path.join(__dirname, "manifest.json"), JSON.stringify(manifest, null, 2));
