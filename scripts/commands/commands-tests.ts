566b359cefc5f3336bc813ba41ec62bdc1422ba4
bot174 <91819282+bot174@users.noreply.github.com>
2022-07-24 11:49:06 +0100

    fix NaN

--- scripts/commands/commands-tests.ts
+++ scripts/commands/commands-tests.ts
@@ -2,10 +2,10 @@ import { Commands } from "./index.ts";
 
 let timerA = Date.now();
 Commands.run("say Hello World");
-console.warn("Commands.run time: " + Date.now() - timerA + " ms");
+console.warn(`Commands.run time: ${Date.now() - timerA} ms`);
 
 let timerB = Date.now();
 Commands.runAsync("say Hello World in async");
-console.warn("Commands.run time: " + Date.now() - timerB + " ms");
+console.warn(`Commands.runAsync time: ${Date.now() - timerB} ms`);
 
 console.log("End of test");
