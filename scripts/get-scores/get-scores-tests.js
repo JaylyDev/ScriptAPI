5375a49ed3201875827f9da3377f4fd4edee4fc5
Jayly <65847850+JaylyDev@users.noreply.github.com>
2022-07-25 16:14:42 +0100

    update import

--- scripts/get-scores/get-scores-tests.js
+++ scripts/get-scores/get-scores-tests.js
@@ -1,4 +1,4 @@
-import { getScore, getScores } from "/index.js";
+import { getScore, getScores } from "./index.js";
 import { world } from "mojang-minecraft";
 
 let player = [...world.getPlayers()][0];
