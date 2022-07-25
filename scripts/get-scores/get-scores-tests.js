ea4626fa9ea7f1c3e1a2bd0807d76c9296763bb6
bot174 <91819282+bot174@users.noreply.github.com>
2022-07-25 13:35:00 +0100

    Create get-scores-tests.js

---
+++ scripts/get-scores/get-scores-tests.js
@@ -0,0 +1,8 @@
+import { getScore, getScores } from "/index.js";
+import { world } from "mojang-minecraft";
+
+let player = [...world.getPlayers()][0];
+
+getScores(player);
+
+getScore(player, "myObjective");
