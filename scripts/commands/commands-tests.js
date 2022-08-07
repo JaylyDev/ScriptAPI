0fd6fd2c75f002b2c6709da9519de1e36210ae65
Jayly <65847850+JaylyDev@users.noreply.github.com>
2022-08-07 17:38:24 +0800

    Add Commands.register method

---
+++ scripts/commands/commands-tests.js
@@ -0,0 +1,36 @@
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+import { Commands } from "./index.js";
+var timerA = Date.now();
+Commands.run("say Hello World");
+console.warn("Commands.run time: ".concat(Date.now() - timerA, " ms"));
+var timerB = Date.now();
+Commands.runAsync("say Hello World in async");
+console.warn("Commands.runAsync time: ".concat(Date.now() - timerB, " ms"));
+Commands.register("#", "sayhi", function (res) {
+    res.player.runCommand("say hi ".concat(__spreadArray([], __read(res.argv), false).join(", ")));
+});
+console.log("End of test");
--- scripts/commands/index.js
+++ scripts/commands/index.js
@@ -1,10 +1,118 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __values = (this && this.__values) || function(o) {
+    var s = typeof Symbol === "function" && Symbol.iterator, m = s && o[s], i = 0;
+    if (m) return m.call(o);
+    if (o && typeof o.length === "number") return {
+        next: function () {
+            if (o && i >= o.length) o = void 0;
+            return { value: o && o[i++], done: !o };
+        }
+    };
+    throw new TypeError(s ? "Object is not iterable." : "Symbol.iterator is not defined.");
+};
 import { Dimension, Entity, world } from "mojang-minecraft";
+/**
+ * Main class for custom command functions, with the player that execute
+ * this command with additional arguments split in an iterable iterator
+ * string array.
+ */
+var Command = /** @class */ (function () {
+    function Command(argv, player) {
+        this.argv = (function () {
+            var argv_1, argv_1_1, arg, e_1_1;
+            var e_1, _a;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        _b.trys.push([0, 5, 6, 7]);
+                        argv_1 = __values(argv), argv_1_1 = argv_1.next();
+                        _b.label = 1;
+                    case 1:
+                        if (!!argv_1_1.done) return [3 /*break*/, 4];
+                        arg = argv_1_1.value;
+                        return [4 /*yield*/, arg];
+                    case 2:
+                        _b.sent();
+                        _b.label = 3;
+                    case 3:
+                        argv_1_1 = argv_1.next();
+                        return [3 /*break*/, 1];
+                    case 4: return [3 /*break*/, 7];
+                    case 5:
+                        e_1_1 = _b.sent();
+                        e_1 = { error: e_1_1 };
+                        return [3 /*break*/, 7];
+                    case 6:
+                        try {
+                            if (argv_1_1 && !argv_1_1.done && (_a = argv_1.return)) _a.call(argv_1);
+                        }
+                        finally { if (e_1) throw e_1.error; }
+                        return [7 /*endfinally*/];
+                    case 7: return [2 /*return*/];
+                }
+            });
+        })();
+        this.__player = player;
+    }
+    Object.defineProperty(Command.prototype, "player", {
+        get: function () { return this.__player; },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Command.prototype, "argv0", {
+        get: function () { return this.argv.next().value; },
+        enumerable: false,
+        configurable: true
+    });
+    ;
+    ;
+    return Command;
+}());
+export { Command };
+;
 /**
  * Contains a method that lets you run console commands within
  * Minecraft.
  */
 // tslint:disable-next-line:no-unnecessary-class
-export class Commands {
+var Commands = /** @class */ (function () {
+    function Commands() {
+    }
     /**
      * @remarks
      * Runs a particular command from the context.
@@ -22,12 +130,13 @@ export class Commands {
      *        Commands.run("scoreboard players set @p score 10", world.getDimension("overworld"));
      * ```
      */
-    static run(commandString, target = world.getDimension("overworld")) {
+    Commands.run = function (commandString, target) {
+        if (target === void 0) { target = world.getDimension("overworld"); }
         if (target instanceof Dimension || Entity)
             return target.runCommand(commandString);
         else
             throw TypeError("Native type conversion failed");
-    }
+    };
     ;
     /**
      * @remarks
@@ -46,12 +155,61 @@ export class Commands {
      * an indicator of command results.
      * @throws This function can throw errors.
      */
-    static async runAsync(commandString, target = world.getDimension("overworld")) {
-        if (target instanceof Dimension || Entity)
-            return target.runCommandAsync(commandString);
-        else
-            throw TypeError("Native type conversion failed");
-    }
+    Commands.runAsync = function (commandString, target) {
+        if (target === void 0) { target = world.getDimension("overworld"); }
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                if (target instanceof Dimension || Entity)
+                    return [2 /*return*/, target.runCommandAsync(commandString)];
+                else
+                    throw TypeError("Native type conversion failed");
+                return [2 /*return*/];
+            });
+        });
+    };
     ;
-}
+    /**
+     * @remarks
+     * Registers a new custom command. This command will become
+     * available in Minecraft via [prefix][command].
+     * @param prefix
+     * The prefix of this specific command. (Case sensitive)
+     * @param command
+     * Name of this specific command. (Case sensitive)
+     * @param commandFunction
+     * Implementation of the command function.
+     * @throws
+     * This function can throw error: You are not allow to register a new slash command.
+     * @example example1.js
+     * ```typescript
+     *          Commands.register("!", "test", function (arg) {
+     *              arg.player.runCommand(`say ${arg.argv0} ${JSON.stringify([...arg.argv])}`);
+     *          });
+     * ```
+     */
+    Commands.register = function (prefix, command, commandFunction) {
+        if (prefix.startsWith("/"))
+            throw Error("Unable to register slash commands.");
+        world.events.beforeChat.subscribe(function (arg) {
+            var argv = arg.message.split(/(".*?"|[^"\s]+)+(?=\s*|\s*$)/g).filter(function (e) { return e.trim().length > 0; });
+            if (argv[0] === "".concat(prefix).concat(command)) {
+                arg.cancel = true;
+                try {
+                    commandFunction(new Command(argv, arg.sender));
+                }
+                catch (err) {
+                    var statusMessage = JSON.parse(err).statusMessage;
+                    console.error(err);
+                    // @ts-ignore
+                    !!arg.sender.tell ? arg.sender.tell("\u00A7c".concat(statusMessage)) : arg.sender.runCommand("tellraw @s {\"rawtext\":[{\"text\": \"\u00A7c".concat(statusMessage, "\"}]}"));
+                }
+                ;
+            }
+            ;
+        });
+    };
+    ;
+    return Commands;
+}());
+export { Commands };
 ;
