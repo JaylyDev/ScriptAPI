6c348f84d02d4b45449f08ea090a0beeffa39a47
bigguy69420 <itsdominicplays@gmail.com>
2022-08-14 21:47:51 +0800

    Update http-request.js

--- mojang-net/http-request/http-request.js
+++ mojang-net/http-request/http-request.js
@@ -1,4 +1,4 @@
-import { http, HttpRequest } from "mojang-net";
+import { http, HttpHeader, HttpRequest, HttpRequestMethod } from "mojang-net";
 
 /**
  * URL await to request
@@ -7,4 +7,36 @@ const uri = "https://minecraft.net/"
 
 let config = new HttpRequest(uri);
 
-http.request(config)
+// Add header
+config.addHeader("key", "value");
+
+// set multiple headers
+config.setHeaders([new HttpHeader("key", "value"), new HttpHeader("foo", "bar")]);
+
+// get value from secrets
+import { secrets } from "mojang-minecraft-server-admin";
+
+let secretToken = secrets.get("token");
+
+config.setHeaders([new HttpHeader("myToken", secretToken)]);
+
+// Get/Set request method
+config.method = HttpRequestMethod.GET;
+config.setMethod(HttpRequestMethod.GET);
+
+// set config body (if neccessary)
+config.body = "Hi mom";
+
+http.request(config).then((response) => {
+  // Body content of the HTTP response.
+  const body = response.body;
+  // A collection of HTTP response headers returned from the request.
+  const headers = response.headers;
+  // The HTTP request that corresponds to the HTTP response that this object represents.
+  const request = response.request;
+  // HTTP response code for the request.
+  // For more info check https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+  const status = response.status;
+
+  // code here
+});
