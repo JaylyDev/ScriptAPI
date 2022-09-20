728e81f119ef479229e1e7273c20206d860a797c
Jayly <65847850+JaylyDev@users.noreply.github.com>
2022-09-20 17:00:02 +0100

    add package run-commands

---
+++ scripts/run-commands/run-commands-tests.js
@@ -0,0 +1,20 @@
+import { BlockLocation, world } from 'mojang-minecraft';
+import { runCommands, runCommandsAsync } from './index.js';
+
+// spawns a zombie then runs commands 
+runCommands(
+    world.getDimension('overworld').spawnEntity('minecraft:zombie', new BlockLocation(0, 64, 0)),
+    'say Hello World!',
+    'scoreboard players add @s zombies 1'
+);
+
+// run commands on a player
+runCommands([...world.getPlayers()][0], 'clear @s stone');
+
+// run commands on a dimension or script engine
+runCommandsAsync(
+    world.getDimension('overworld'),
+    'teleport @a 0 64 0',
+    'effect @e night_vision 10 0',
+    'say Dimension'
+);
