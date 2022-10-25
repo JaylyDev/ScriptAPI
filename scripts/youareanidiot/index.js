312defc63cd9df0005d947b3cdd7fa2211affee9
Jayly <65847850+JaylyDev@users.noreply.github.com>
2022-10-25 14:33:58 +0100

    runCommand -> runCommandAsync

--- scripts/youareanidiot/index.js
+++
@@ -1,8 +0,0 @@
-import { setInterval } from "../timers/timers.js";
-import { world } from "mojang-minecraft";
-
-export function YouAreAnIdiot () {
-  setInterval(() => {
-    world.getDimension("overworld").runCommandAsync("say You are an idiot hahahhahahhaha")
-  }, 1000)
-};
