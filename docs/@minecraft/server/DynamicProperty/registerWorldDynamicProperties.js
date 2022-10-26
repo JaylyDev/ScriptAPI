c076b1a4b05585e641428ad921a1819ecb3a129a
Jayly <65847850+JaylyDev@users.noreply.github.com>
2022-10-26 10:49:06 +0100

    gh actions fix, readme update
    Close #31 and #34

--- docs/@minecraft/server/DynamicProperty/registerWorldDynamicProperties.js
+++ docs/@minecraft/server/DynamicProperty/registerWorldDynamicProperties.js
@@ -20,6 +20,6 @@ world.setDynamicProperty("my_boolean", true);
 world.setDynamicProperty("my_number", 100);
 world.setDynamicProperty("my_string", 'Hi mom');
 
-world.getDynamicProperty("my_boolean");
-world.getDynamicProperty("my_number");
-world.getDynamicProperty("my_string");
+world.getDynamicProperty("my_boolean"); // true
+world.getDynamicProperty("my_number"); // 100
+world.getDynamicProperty("my_string"); // 'Hi mom'
