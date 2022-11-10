ecd380b9d3f16d49c15777d04703d87d8f57dcf9
bot174 <91819282+bot174@users.noreply.github.com>
2022-11-10 08:07:41 +0000

    Update tests.js

--- scripts/force-show/tests.js
+++ scripts/force-show/tests.js
@@ -1,5 +1,5 @@
-import { world } from "@minecraft/server";
-import { ModalFormData } from "@minecraft/server-ui";
+import { world, system } from "@minecraft/server";
+import { ModalFormData, ActionFormData, MessageFormData } from "@minecraft/server-ui";
 import { forceShow } from "force-show";
 
 world.events.beforeChat.subscribe(async (event) => {
@@ -11,4 +11,30 @@ world.events.beforeChat.subscribe(async (event) => {
   
   const response = await forceShow(event.sender, modalForm);
   response['formValues']
+});
+
+world.events.beforeItemUse.subscribe(async (event) => {
+  event.cancel = true;
+
+  const form = new ActionFormData();
+  form.button('button');
+  form.button('button');
+  form.button('button');
+  
+  const response = await forceShow(event.source, form);
+});
+
+
+
+system.run(() => {
+  for (let player of world.getAllPlayers()) {
+  event.cancel = true;
+
+  const form = new MessageFormData();
+  form.title('title');
+  form.button1('button');
+  form.button2('button');
+  
+  const response = await forceShow(player, form);
+  };
 });
