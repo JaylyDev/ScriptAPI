38721119bebc48fdda1185ac8929780b4e4ab817
Worldwidebrine <40757969+Worldwidebrine@users.noreply.github.com>
2022-11-10 16:03:17 +0800

    force-show: Add @returns (#42)
    Update index.js

--- scripts/force-show/index.js
+++ scripts/force-show/index.js
@@ -1,19 +1,14 @@
-import { Player } from "@minecraft/server";
-import {
-  ModalFormData,
-  MessageFormData,
-  ActionFormData,
-} from "@minecraft/server-ui";
-
 /**
- * @param {Player} player
- * @param {ModalFormData | MessageFormData | ActionFormData} form
+ * @template {import("@minecraft/server-ui").ActionFormData | import("@minecraft/server-ui").MessageFormData | import("@minecraft/server-ui").ModalFormData} Form
+ * @param {Form} form
+ * @param {import("@minecraft/server").Player} player
+ * @returns {Promise<Awaited<ReturnType<Form["show"]>>>}
  */
 export async function forceShow(player, form) {
-  while (true) {
-    const response = await form.show(player);
-    if (response.cancelationReason !== "userBusy") {
-      return response;
+    while (true) {
+        const response = await /** @type {ReturnType<Form["show"]>} */(form.show(player));
+        if (response.cancelationReason !== "userBusy") {
+            return response;
+        }
     }
-  }
-}
+};
