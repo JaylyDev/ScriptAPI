a7c66417fa7bb3d7d7c9db63e9b77eed4d0dbf62
Jayly <65847850+JaylyDev@users.noreply.github.com>
2022-12-21 16:00:18 +0800

    Add hide-ign

---
+++ scripts/hide-ign/hideign.js
@@ -0,0 +1,35 @@
+// Script examples for ScriptAPI
+// Author: Jayly#1397 <undefined>
+import { world } from "mojang-minecraft";
+
+function hideign() {
+  for (let entity of world.getDimension("overworld").getEntities()) {
+    if (entity.nameTag !== "") {
+      entity.jayly = {
+        nameTag: entity.nameTag,
+      };
+      entity.nameTag = "";
+    }
+  }
+}
+
+world.events.beforeChat.subscribe(callback => {
+  const { message, sender } = callback;
+  const commandmsg = (bool) => `tellraw @s {"rawtext":[{"translate":"commands.gamerule.success","with":["showign", "${bool}"]}]}`;
+
+  if (message.startsWith("!showign true")) {
+    world.events.tick.unsubscribe(hideign);
+    callback.cancel = true;
+    for (let entity of world.getDimension("overworld").getEntities()) {
+      if (entity.jayly) {
+        entity.nameTag = entity.jayly.nameTag;
+        delete entity.jayly;
+      }
+    };
+    sender.runCommand(commandmsg(true));
+  } else if (message.startsWith("!showign false")) {
+    world.events.tick.subscribe(hideign);
+    callback.cancel = true;
+    sender.runCommand(commandmsg(false));
+  };
+});
