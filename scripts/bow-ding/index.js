1a2151744c31ff649ec57f6d3f765ecfd57a07be
GlitchyTurtle <93545476+GlitchyTurtle@users.noreply.github.com>
2022-12-26 16:13:10 -0700

    Fix bow-ding to use an interface.
    Using a constructor no longer works, so I've updated this function for the new update (if the outdated code was intentional, feel free to ignore this).

--- scripts/bow-ding/index.js
+++ scripts/bow-ding/index.js
@@ -2,10 +2,11 @@ import { Player, SoundOptions, world } from "mojang-minecraft";
 
 world.events.projectileHit.subscribe((arg) => {
   if (arg.entityHit?.entity instanceof Player && arg.source instanceof Player) {
-    let soundOption = new SoundOptions();
-    soundOption.volume = 0.4;
-    soundOption.pitch = 0.5;
-
+    const soundOption = {
+        volume: 0.4,
+        pitch: 0.5,
+    }
+    
     arg.source.playSound("random.orb", soundOption);
   };
 });
