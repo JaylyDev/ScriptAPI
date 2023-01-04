150195b30c4a5e7102f127246783dae10afe5ab5
bot174 <91819282+bot174@users.noreply.github.com>
2023-01-04 21:38:46 +0800

    Update index.ts

--- scripts/commands/index.ts
+++ scripts/commands/index.ts
@@ -24,28 +24,6 @@ export class Command {
 export class Commands {
   /**
    * @remarks
-   * Runs a particular command from the context.
-   * @param commandString
-   * Command to run. Note that command strings should not start
-   * with slash.
-   * @param target
-   * Target to be used as context for the command to run
-   * within.
-   * @returns For commands that return data, returns a JSON structure with command response values.
-   * @throws This function can throw errors.
-   * @example commands.js
-   * ```typescript
-   *        Commands.run("say You got a new high score!");
-   *        Commands.run("scoreboard players set @p score 10", world.getDimension("overworld"));
-   * ```
-   */
-  static run(commandString: string, target: Dimension | Entity = world.getDimension("overworld")): any {
-    if (target instanceof Dimension || Entity) return target.runCommandAsync(commandString);
-    else throw TypeError("Native type conversion failed");
-  };
-
-  /**
-   * @remarks
    * Runs a particular command asynchronously from the context.
    * Where possible - and especially for
    * long-running operations - you should use runCommandAsync
@@ -62,7 +40,7 @@ export class Commands {
    * @throws This function can throw errors.
    */
   static async runAsync(commandString: string, target: Dimension | Entity = world.getDimension("overworld")): Promise<CommandResult> {
-    if (target instanceof Dimension || Entity) return target.runCommandAsync(commandString);
+    if (target instanceof Dimension || Entity) return await target.runCommandAsync(commandString);
     else throw TypeError("Native type conversion failed");
   };
 
