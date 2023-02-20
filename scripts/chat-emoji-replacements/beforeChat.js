96d607fb25fa4b62002ab0574bf7e29413f31aa3
GlitchyTurtle <coop@gordross.com>
2023-02-20 12:20:06 -0700

    Remove redundant getScore.

--- scripts/chat-emoji-replacements/beforeChat.js
+++ scripts/chat-emoji-replacements/beforeChat.js
@@ -9,16 +9,15 @@ const replacements = {
 
 function beforeChat(msg) {
     const player = msg.sender;
-    if (!getScore("chatRankSet", player)) {
-        let message = msg.message;
-        let regex = new RegExp(":" + Object.keys(replacements).join(":|:") + ":", "g");
 
-	 // Replaces the word with emoji if it has :: around it. :armor: -> 
-        message = message.replace(regex, (match) => { return replacements[match.replaceAll(":", "")] + "§r" });
+    let message = msg.message;
+    let regex = new RegExp(":" + Object.keys(replacements).join(":|:") + ":", "g");
+    
+    // Replaces the word with emoji if it has :: around it. :armor: -> 
+    message = message.replace(regex, (match) => { return replacements[match.replaceAll(":", "")] + "§r" });
 
-        world.sendMessage(`<${player.name}> ${message.replace(/\\"/g, '"')}`);
-        msg.cancel = true;
-    }
+    world.sendMessage(`<${player.name}> ${message.replace(/\\"/g, '"')}`);
+    msg.cancel = true;
 }
 
 world.events.beforeChat.subscribe(msg => beforeChat(msg));
