97d41bfd95262cb36f90bdd48d8ec53462bf57e0
GamerFile <107175636+GamerFile@users.noreply.github.com>
2023-03-21 19:49:17 +0530

    Update index.js
    • Updated .setObj method.
    • Improved Js Docs

--- scripts/scoreboard-m/index.js
+++ scripts/scoreboard-m/index.js
@@ -1,32 +1,60 @@
 // Script example for ScriptAPI
 // Author: GamerFile <https://github.com/GamerFile>
 // Project: https://github.com/JaylyDev/ScriptAPI
-
 import * as mc from '@minecraft/server'
 
 //The Main ScoreboardM Object For Managing scoreboard
- export const ScoreboardM = {
-     /**
-      * This Is A Method For Setting An Objective Different displayName
-      * @param {string} id - The Id Of Objective
-      * @param {string} v - The Name To Be Displayed
-      * @returns {ScoreboardM} - Returns The Main Object
-      */
-    setObj : function(id, v) {
+let objOptions = [],list;
+
+export const ScoreboardM = {
+    /**
+     * This Is A Method For Setting An Objective Different displayName
+     * @param {string} id - The Id Of Objective
+     * @param {string} v - The Name To Be Displayed
+     * @returns {ScoreboardM} - Returns The Main Object
+     */
+    setObj: function(id, v) {
         if (this.getObj(id)) {
-            this.delObj(id)
+            objOptions[0] = this.getDisplaySlot("sidebar") ? this.getDisplaySlot("sidebar") : undefined;
+
+            objOptions[1] = this.getDisplaySlot("list") ? this.getDisplaySlot("list") : undefined;
+
+            objOptions[2] = this.getDisplaySlot("belowName") ? this.getDisplaySlot("belowName") : undefined;
+
+            list = this.getList(id)
+            this.delObj(id);
             this.setObj(id, v)
+
         } else {
-            mc.world.scoreboard.addObjective(id, v)
+            mc.world.scoreboard.addObjective(id, v);
+            for (let ind in objOptions) {
+                let opts = objOptions[ind]
+                if (opts?.objective.id == id) {
+                    list?.forEach(obj => {
+                        this.setScore(obj, id, obj.score)
+                    })
+                    switch (ind) {
+                        case "0":
+                            this.setDisplaySlot("sidebar", opts);
+                            break;
+                        case "1":
+                            this.setDisplaySlot("list", opts);
+                            break;
+                        case "2":
+                            this.setDisplaySlot("belowName", opts);
+                            break;
+                    }
+                }
+            }
         }
         return this;
     },
-         /**
-      * This method tells whether a objective exist or not
-      * @param {string} id - The Id Of Objective
-      * @returns {boolean} - Returns True Or False , If Objective Exists then
-      */
-    hasObj : function(id) {
+    /**
+     * This method tells whether a objective exist or not
+     * @param {string} id - The Id Of Objective
+     * @returns {boolean} - Returns True Or False , If Objective Exists then
+     */
+    hasObj: function(id) {
         if (this.getObj(id)) {
             return true
 
@@ -34,38 +62,38 @@ import * as mc from '@minecraft/server'
             return false
         }
     },
-         /**
-      * This Method For Getting Objective Object
-      * @param {string} id - The Id Of Objective
-      * @returns {Object} - Returns The Objective Object
-      */
-    getObj : function(id) {
+    /**
+     * This Method For Getting Objective Object
+     * @param {string} id - The Id Of Objective
+     * @returns {mc.scoreboardObjective} - Returns The Objective Object
+     */
+    getObj: function(id) {
         if (mc.world.scoreboard.getObjective(id)) {
             return mc.world.scoreboard.getObjective(id)
         }
     },
-         /**
-      * This Is A Method For Creating  An Objective With The Display Name
-      * @param {string} id - The Id Of Objective
-      * @param {string} v - The Name To Be Displayed
-      * @returns {ScoreboardM} - Returns The Main Object
-      */
-    newObj : function(id, v) {
+    /**
+     * This Is A Method For Creating  An Objective With The Display Name
+     * @param {string} id - The Id Of Objective
+     * @param {string} v - The Name To Be Displayed
+     * @returns {ScoreboardM} - Returns The Main Object
+     */
+    newObj: function(id, v) {
         if (this.getObj(id)) {
             throw new Error("Objective Already exist. Error At newObj()")
             return
         }
         return this.setObj(id, v)
     },
-         /**
-      * This Is A Method For Setting Target's Score
-      * @param {Object} target - The Target's Object To Set Score
-      * @param {string} Objective - The Id Of Objective
-      * @param {number} score - Score To Be Setted To Target
-      * @returns {ScoreboardM} - Returns The Main Object
-      * @throws {Error} - Throws Error When Objective Doesnt Exist
-      */
-    setScore : function(target, Objective, score) {
+    /**
+     * This Is A Method For Setting Target's Score
+     * @param {{name: string}} target - The Target's Object To Set Score
+     * @param {string} Objective - The Id Of Objective
+     * @param {number} score - Score To Be Setted To Target
+     * @returns {ScoreboardM} - Returns The Main Object
+     * @throws {Error} - Throws Error When Objective Doesnt Exist
+     */
+    setScore: function(target, Objective, score) {
         if (this.getObj(Objective)) {
             mc.world.getDimension("overworld").runCommandAsync(`scoreboard players set ${target.name} "${Objective}" ${Number(score)}`)
             return this
@@ -73,14 +101,14 @@ import * as mc from '@minecraft/server'
             throw new Error("Objective Doesnt exist . At setScore()")
         }
     },
-             /**
-      * This Is A Method For Getting Target's Score
-      * @param {Object} target - The Target's Object To Get Score
-      * @param {string} Objective - The Id Of Objective
-      * @returns {number} - Returns The Target's Score
-      * @throws {Error} - Throws Error When Objective Doesnt Exist
-      */
-    getScore : function(target, Objective) {
+    /**
+     * This Is A Method For Getting Target's Score
+     * @param {{name: string}} target - The Target's Object To Get Score
+     * @param {string} Objective - The Id Of Objective
+     * @returns {number} - Returns The Target's Score
+     * @throws {Error} - Throws Error When Objective Doesnt Exist
+     */
+    getScore: function(target, Objective) {
         if (this.getObj(Objective)) {
             var obj = mc.world.scoreboard.getObjective(Objective)
             var players = obj.getParticipants()
@@ -96,15 +124,14 @@ import * as mc from '@minecraft/server'
             throw new Error("Objective Doesn't exist. at getScore()")
         }
     },
-                 /**
-      * This Is A Method For Adding Score To Target
-      * @param {Object} target - The Target's Object To Add Score
-      * @param {string} Objective - The Id Of Objective
-      * @param {number} score - The Number To Add
-      * @returns {ScoreboardM} - Returns The Main ScoreboardM Object
-      * @throws {Error} - Throws Error When Objective Doesnt Exist
-      */
-    addScore : function(target, Objective, score) {
+    /**
+     * This Is A Method For Adding Score To Target
+     * @param {{name: string}} target - The Target's Object To Add Score
+     * @param {string} Objective - The Id Of Objective
+     * @returns {ScoreboardM} - Returns The Main ScoreboardM Object
+     * @throws {Error} - Throws Error When Objective Doesnt Exist
+     */
+    addScore: function(target, Objective, score) {
         if (this.getObj(Objective)) {
             mc.world.getDimension("overworld").runCommandAsync(`scoreboard players add ${target.name} "${Objective}" ${score}`)
             return this
@@ -112,12 +139,27 @@ import * as mc from '@minecraft/server'
             throw new Error("Objective Doesn't exist. at addScore()")
         }
     },
-             /**
-      * This Method For Deleting Objective Object
-      * @param {string} id - The Id Of Objective
-      * @returns {Object} - Returns The Main ScoreboardM Object
-      */
-    delObj : function(id) {
+    /**
+     * This Method Is For Removing a entity,fakeplayer or player name from given objective
+     * @param {{name: string}} target - The Object Of The Player Or Fakeplayer
+     * #param {string} Objective - Id Of The Objective
+     * @returns {ScoreboardM} - Returns The Main ScoreboardM Object
+     * @throws {Error} - Throws Error When Objective Doesnt Exist
+     */
+    removeName: function(target,Objective) {
+        if (this.hasObj(Objective)) {
+            mc.world.getDimension("overworld").runCommandAsync(`scoreboard players reset ${target.name} "${Objective}"`)
+            return this;
+        } else {
+            throw new Error("Objective Doesn't exist. at removeName()")
+        }
+    },
+    /**
+     * This Method For Deleting Objective Object
+     * @param {string} id - The Id Of Objective
+     * @returns {ScoreboardM} - Returns The Main ScoreboardM Object
+     */
+    delObj: function(id) {
         if (this.hasObj(id)) {
             mc.world.scoreboard.removeObjective(id)
             return this;
@@ -129,7 +171,7 @@ import * as mc from '@minecraft/server'
      * @param {string} Objective - Id Of Objective To Get Names
      * @returns {String[] | undefined} - Contains All Names and also undefined it there isnt any of that score
      */
-    getNameByScore : function(score, Objective) {
+    getNameByScore: function(score, Objective) {
         if (this.hasObj(Objective)) {
 
             let obj = this.getObj(Objective)
@@ -157,12 +199,12 @@ import * as mc from '@minecraft/server'
         }
 
     },
-        /**
+    /**
      * This Method Is To Get An Array Of Names And Scores Having. Could Work On Fakeplayers
      * @param {string} obj - Id Of Objective To Get Names And Scores
-     * @returns {Object[] | undefined} - Contains All Names And Scores and also undefined it there isnt any of that score
+     * @returns {[{name: string,score: number}...] | undefined} - Contains All Names And Scores and also undefined it there isnt any of that score
      */
-    getList : function(obj) {
+    getList: function(obj) {
 
         if (this.hasObj(obj)) {
 
@@ -190,38 +232,38 @@ import * as mc from '@minecraft/server'
     },
     /**
      * This Method Is To Getting Display Slot Options That Is Currently Displaying
-     * @param {string} id - Id Of Slot To Get Options
-     * @returns {Object} - This Returns Display Options
+     * @param {'sidebar' | 'list' | 'belowName'} id - Id Of Slot To Get Options
+     * @returns {mc.ScoreboardObjectiveDisplayOptions} - This Returns Display Options
      * @example 
      * ScoreboardM.getDisplaySlot("sidebar")
      */
     getDisplaySlot: function(id) {
-        
-            return mc.world.scoreboard.getObjectiveAtDisplaySlot(id);
+
+        return mc.world.scoreboard.getObjectiveAtDisplaySlot(id);
     },
     /**
      * This Clears The Displaying Objective At Given Slot
-     * @param {string} id - The Display Slot Id To Clear
+     * @param {'sidebar'| 'list' | 'belowName'} id - The Display Slot Id To Clear
      * @returns {ScoreboardM} - The Main ScoreboardM Object
      * @example
      * ScoreboardM.clearDisplaySlot("sidebar")
      */
     clearDisplaySlot: function(id) {
-                
-             mc.world.scoreboard.clearObjectiveAtDisplaySlot(id);
-             return this;
+
+        mc.world.scoreboard.clearObjectiveAtDisplaySlot(id);
+        return this;
     },
     /**
      * This Sets/Displays Objective At Given Slots
-     * @param {string} id - The Slot Id To Display Objective
-     * @param {Object} opt - The scoreboard Options to display the Objective
+     * @param {'sidebar'|'list'| 'belowName'} id - The Slot Id To Display Objective
+     * @param {mc.ScoreboardObjectiveDisplayOptions} opt - The scoreboard Options to display the Objective
      * @returns {ScoreboardM} - The Main ScoreboardM Object
      * @example
      * ScoreboardM.setDisplaySlot("sidebar",{objective: ScoreboardM.getObj("money"),sortOrder: 0})
      */
-    setDisplaySlot: function(id,opt) {
-             mc.world.scoreboard.setObjectiveAtDisplaySlot(id,opt);
-             return this;
+    setDisplaySlot: function(id, opt) {
+        mc.world.scoreboard.setObjectiveAtDisplaySlot(id, opt);
+        return this;
 
     }
 }
