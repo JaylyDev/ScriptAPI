b4342240450ac0ee02e2eb03877c93990db5eac3
FrankyRayMS <61337607+FrankyRay@users.noreply.github.com>
2023-03-30 18:57:26 +0700

    Create index.js

---
+++ scripts/get-effects/index.js
@@ -0,0 +1,31 @@
+import {
+  world,
+  MinecraftEffectTypes,
+  /* Typings only */
+  Entity,
+  Player,
+  Effect,
+  EffectType
+} from "@minecraft/server";
+
+/**
+ * Get every effect from the entity
+ *
+ * @param {Entity|Player} entity
+ * Entity or Player class
+ *
+ * @return {{ effectType: EffectType, effect: Effect }[]}
+ */
+export function getEffects(entity) {
+  const effectList = [];
+  for (const eff of Object.values(MinecraftEffectTypes)) {
+    const effect = entity.getEffect(eff);
+    if (!effect) continue;
+    effectList.push({
+      effectType: eff,
+      effect: effect
+    });
+  };
+  
+  return effectList;
+};
