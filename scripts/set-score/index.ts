67a9e3c2561092e56dd64339def8fa7a7d461ac3
JaylyDev <65847850+JaylyDev@users.noreply.github.com>
2023-04-13 12:25:41 +0200

    Update index.ts

--- scripts/set-score/index.ts
+++ scripts/set-score/index.ts
@@ -51,31 +51,24 @@ function setScore (entity: Entity, objectiveId: string, score: number, action?: 
   const objective = world.scoreboard.getObjective(objectiveId);
   if (!objective) throw new ReferenceError('Scoreboard objective does not exist in world.');
 
+  const previousScore = !!entity.scoreboardIdentity ? entity.scoreboardIdentity.getScore(objective) : 0;
+
   switch (action) {
     case ScoreboardAction.add:
-      // If entity doesnt have scoreboard property, run command
-      if (!entity.scoreboardIdentity) entity.runCommandAsync('scoreboard players add @s ' + objective + ' ' + score);
-      else {
-        const previousScore = entity.scoreboardIdentity.getScore(objective);
-        entity.scoreboardIdentity.setScore(objective, previousScore + score);
-      };
+      score += previousScore;
       break;
   
     case ScoreboardAction.remove:
-      // If entity doesnt have scoreboard property, run command
-      if (!entity.scoreboardIdentity) entity.runCommandAsync('scoreboard players remove @s ' + objective + ' ' + score);
-      else {
-        const previousScore = entity.scoreboardIdentity.getScore(objective);
-        entity.scoreboardIdentity.setScore(objective, previousScore - score);
-      };
+      score -= previousScore;
       break;
   
     default:
-      // If entity doesnt have scoreboard property, run command
-      if (!entity.scoreboardIdentity) entity.runCommandAsync('scoreboard players set @s ' + objective + ' ' + score);
-      else entity.scoreboardIdentity.setScore(objective, score);
       break;
   }
+
+  // If entity doesnt have scoreboard property, run command
+  if (!entity.scoreboardIdentity) entity.runCommand('scoreboard players set @s ' + objective + ' ' + score);
+  else entity.scoreboardIdentity.setScore(objective, score);
 
   if (fetch) updateDisplay(objective);
 };
