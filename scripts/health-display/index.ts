462afb679126c1c6216a98df245e6f7c9809b94a
xKingDark <47607185+DarkGamerYT@users.noreply.github.com>
2023-05-26 08:33:50 +0100

    Updated the scripts to work in 1.20.10.20 (#246)
    * Update index.ts
    
    * Update index.ts
    
    * Update index.ts
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update tests.js

--- scripts/health-display/index.ts
+++ scripts/health-display/index.ts
@@ -7,6 +7,6 @@ import { EntityHealthComponent, system, world } from "@minecraft/server";
 system.runInterval(() => {
   for (const player of world.getPlayers()) {
     const health = player.getComponent(EntityHealthComponent.componentId) as EntityHealthComponent;
-    player.nameTag = `${player.name}\n§c❤️ ${health.current.toFixed(1)}`;
+    player.nameTag = `${player.name}\n§c❤️ ${health.currentValue.toFixed(1)}`;
   }
 });
