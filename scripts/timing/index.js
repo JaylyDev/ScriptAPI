3dc62ca304e350eac77da9ce59de0cc84a10aea4
bot174 <91819282+bot174@users.noreply.github.com>
2023-07-30 18:05:38 +0800

    Update index.js

--- scripts/timing/index.js
+++ scripts/timing/index.js
@@ -1,6 +1,7 @@
 // Script example for ScriptAPI
 // Author: bot174 <https://github.com/bot174>
 // Project: https://github.com/JaylyDev/ScriptAPI/
+// @ts-nocheck
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
   function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
   return new (P || (P = Promise))(function (resolve, reject) {
