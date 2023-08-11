6cf44c304b0556d547d99b28440aef9abdbd5258
bot174 <91819282+bot174@users.noreply.github.com>
2023-08-12 05:27:26 +0800

    Update tests.ts

--- scripts/spawn-simulated-player/tests.ts
+++ scripts/spawn-simulated-player/tests.ts
@@ -1,6 +1,5 @@
 import {
   world,
-  MinecraftItemTypes,
   ItemStack,
 } from "@minecraft/server";
 import { MinecraftEffectTypes } from "@minecraft/vanilla-data";
@@ -8,12 +7,14 @@ import { SpawnSimulatedPlayer } from "./index.js";
 
 let host = [...world.getPlayers()][0];
 
+const money = world.scoreboard.getObjective("money") ?? world.scoreboard.addObjective("money", "money");
+
 SpawnSimulatedPlayer(host, function (simulatedPlayer) {
   simulatedPlayer.addEffect(MinecraftEffectTypes.Absorption, 1);
   simulatedPlayer.attack();
   simulatedPlayer.dimension.createExplosion(simulatedPlayer.location, 5);
-  simulatedPlayer.giveItem(new ItemStack(MinecraftItemTypes.acaciaBoat));
-  simulatedPlayer.runCommandAsync("scoreboard players add @s money 1");
+  simulatedPlayer.giveItem(new ItemStack("minecraft:acacia_boat"));
+  money.addScore(simulatedPlayer, 1);
   simulatedPlayer.teleport({ x: 0, y: 0, z: 0 }, { dimension: simulatedPlayer.dimension, rotation: { x: 0, y: 0 }});
   simulatedPlayer.despawn();
 });
