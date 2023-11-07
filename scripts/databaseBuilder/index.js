0a9141715f6ba82ce7ccfb164383d09bd031a943
defowler2005 <129338316+defowler2005@users.noreply.github.com>
2023-11-07 21:00:38 +1030

    Update index.js

--- scripts/databaseBuilder/index.js
+++ scripts/databaseBuilder/index.js
@@ -1,3 +1,16 @@
+// Script example for ScriptAPI
+// Author: defowler2005 <https://github.com/defowler2005>
+// Project: https://github.com/JaylyDev/ScriptAPI
+
+/**
+ * Minecraft Bedrock ScriptAPI Example
+ * @author defowler2005
+ * @version 1.0.0
+ * ---------------------------------------------------------------------------
+ * Add a databaseBuilder to your world, Simple creation!
+ * ---------------------------------------------------------------------------
+ */
+
 import { world } from '@minecraft/server';
 
 /**
