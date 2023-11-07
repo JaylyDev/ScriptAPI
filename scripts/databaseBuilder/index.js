bcbe01e2f31f062f1d8ce1f981cbdaa6d0cbb165
defowler2005 <129338316+defowler2005@users.noreply.github.com>
2023-11-07 20:59:37 +1030

    Create index.js

---
+++ scripts/databaseBuilder/index.js
@@ -0,0 +1,47 @@
+import { world } from '@minecraft/server';
+
+/**
+ * A class for interacting with a database.
+ * @class
+ */
+
+class databaseBuilder {
+    /**
+     * Constructs a new databaseBuilder instance.
+     * @constructor
+     */
+    constructor() { }
+
+    /**
+     * Sets a dynamic property in the Minecraft server world.
+     * @param {string} key - The key for the dynamic property.
+     * @param {number|boolean|string} value - The value to be stored.
+     */
+    set(key, value) {
+        world.setDynamicProperty(`${key}`, value);
+    }
+
+    /**
+     * Gets the value of a dynamic property from the Minecraft server world.
+     * @param {string} key - The key for the dynamic property.
+     * @returns {number|boolean|string|null} The value of the dynamic property, or null if it doesn't exist.
+     */
+    get(key) {
+        if (!world.getDynamicProperty(`${key}`)) {
+            //console.warn(`Dynamic property: ${key} does not exist!`);
+            return null;
+        }
+        return world.getDynamicProperty(`${key}`);
+    }
+
+    /**
+     * Deletes a dynamic property from the Minecraft server world.
+     * @param {string} key - The key for the dynamic property.
+     */
+    delete(key) {
+        if (!world.getDynamicProperty(`${key}`)) return console.warn(`Dynamic property: ${key} does not exist!`);
+        world.setDynamicProperty(`${key}`, undefined);
+    }
+};
+
+export const databaseBuild = new databaseBuilder();
