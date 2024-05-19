805c5afa81534ec940e30d4d87703bad10215490
Jayly <65847850+JaylyDev@users.noreply.github.com>
2024-05-19 20:27:33 +0100

    Remove docs directory

--- docs/JavaScript/print.md
+++
@@ -1,8 +0,0 @@
-# print()
-
-For general output of logging information, can only be displayed in Content log file and debugging consoles.
-
-## Syntax
-```js
-print("Hello World")
-```
--- docs/MinecraftApi/@minecraft/server/watchdog/beforeWatchdogTerminate.js
+++
@@ -1,7 +0,0 @@
-import { system } from "@minecraft/server";
-
-// Cancel termination of a script runtime due to a violation of the performance
-// watchdog system.
-system.events.beforeWatchdogTerminate.subscribe((eventData) => {
-  eventData.cancel = true;
-});
